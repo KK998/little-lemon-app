22bb77cfda7db9b7a4a7fd3076ce8be12aa1700f
Klemen Komel <klemen.komel@razum.si>
2024-01-15 14:31:02 +0100

    add color util for brand colors

---
+++ src/util/colors.ts
@@ -0,0 +1,26 @@
+export default {
+    hex: {
+        primary: {
+            green: "#495E57",
+            yellow: "#F4CE14",
+        },
+        secondary: {
+            orange: "#EE9972",
+            lightBrown: "#FBDABB",
+            white: "#EDEFEE",
+            black: "#333333",
+        }
+    },
+    class: {
+        primary: {
+            green: "bg-[#495E57]",
+            yellow: "bg-[#F4CE14]",
+        },
+        secondary: {
+            orange: "bg-[#EE9972]",
+            lightBrown: "bg-[#FBDABB]",
+            white: "bg-[#EDEFEE]",
+            black: "bg-[#333333]",
+        },
+    }
+} as const;
