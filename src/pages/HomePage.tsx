143adc9b40aea7ec36daac8615b84c27fa266fd4
Klemen Komel <klemen.komel@razum.si>
2024-01-15 17:11:23 +0100

    home page hero section and configured default tailwind container sizes

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,52 @@
+import { Link } from "react-router-dom";
+import Button from "../components/Button";
+import Typography from "../components/Typography";
 import Default from "../layouts/Default";
+import colors from "../util/colors";
+import cn from "../util/cn";
+import Container from "../components/Container";
+
+import HomePageHero from "../assets/HomePageHero.svg";
+
+function HeroSection() {
+    return (
+        <section className={cn("flex items-center justify-center py-10 mb-20", colors.bg.primary.green)}>
+            <Container className="relative">
+                <div className="flex flex-col gap-5 max-w-full sm:max-w-sm mr-auto">
+                    <div className="flex flex-col gap-0">
+                        <Typography tag={"h1"} type="Display title" className={colors.text.primary.yellow}>
+                            Little Lemon
+                        </Typography>
+                        <Typography tag={"h3"} type="Sub title" className={colors.text.secondary.white}>
+                            Chicago
+                        </Typography>
+                    </div>
+                    <Typography type="Lead text" className={colors.text.secondary.white}>
+                        We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.
+                    </Typography>
+                    <Button kind="yellow">
+                        <Link to={"/reservations"}>
+                            Reserve a table
+                        </Link>
+                    </Button>
+                </div>
+                <img
+                    height={`353px`}
+                    width={`380px`}
+                    loading="eager"
+                    className="absolute right-0 top-0"
+                    src={HomePageHero}
+                    alt="Man with food"
+                />
+            </Container>
+        </section>
+    );
+}
 
 function HomePage() {
     return (
         <Default>
-            <h1>Little Lemon Restaurant</h1>
+            <HeroSection />
         </Default>
     );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,19 @@ module.exports = {
     "./src/**/*.{ts,tsx}"
   ],
   theme: {
+    container: {
+      /* // you can configure the container to be centered
+      center: true, */
+
+      // or have default horizontal padding
+      padding: '1rem',
+
+      screens: {
+        sm: '100%',
+        md: '728px',
+        lg: '861px',
+      },
+    },
     extend: {},
   },
   plugins: [],
